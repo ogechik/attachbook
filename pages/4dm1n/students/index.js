a474a794242fd50467b3c2403555658518d1c0a2
reptile <kevinogechi798@gmail.com>
2022-04-06 20:25:27 +0300

    admin student page

---
+++ pages/4dm1n/students/index.js
@@ -0,0 +1,90 @@
+import { Divider, Typography, Row, Col, List } from 'antd'
+import AdminLayout from '../../../components/admin/AdminLayout'
+const { Title, Text } = Typography
+
+export default function Companies({ students }) {
+  return (
+    <AdminLayout title="Admin|Students">
+      <Row>
+        <Col span={24}>
+          <Title level={3}>Students</Title>
+        </Col>
+      </Row>
+      <Divider />
+
+      <Row>
+        <Col span={24}>
+          <List
+            header={
+              <div>
+                <Row style={{ width: '100%' }}>
+                  <Col span={6}>
+                    <Text strong>Name</Text>
+                  </Col>
+                  <Col span={6}>
+                    <Text strong>Email</Text>
+                  </Col>
+                </Row>
+              </div>
+            }
+            footer={
+              <div>
+                <Text strong>{students.length} students</Text>
+              </div>
+            }
+            bordered
+            dataSource={students}
+            renderItem={(student) => (
+              <List.Item>
+                <Row style={{ width: '100%' }}>
+                  <Col span={6}>
+                    {`${student.firstName} ${student.lastName}`}
+                  </Col>
+                  <Col span={6}>{student.email}</Col>
+                </Row>
+              </List.Item>
+            )}
+          />
+        </Col>
+      </Row>
+    </AdminLayout>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const { cookie } = context.req.headers
+  if (!cookie) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/',
+      },
+    }
+  }
+
+  const response = await fetch(`${process.env.DOMAIN}/api/a/students`, {
+    headers: { cookie },
+  })
+  if (response.status === 200) {
+    const students = await response.json()
+    return {
+      props: {
+        students,
+      },
+    }
+  }
+  if (response.status === 401) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/4dm1n',
+      },
+    }
+  }
+
+  return {
+    props: {
+      students: [],
+    },
+  }
+}
