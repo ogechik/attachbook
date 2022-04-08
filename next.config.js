6d7c67d9d82488b309e676827a0409d3cc5ebae0
reptile <kevinogechi798@gmail.com>
2022-04-09 00:30:22 +0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,32 @@
 /** @type {import('next').NextConfig} */
+
+const withPlugins = require('next-compose-plugins')
+const withLess = require('next-with-less')
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
-
-// antd less config
-const withLess = require('next-with-less')
-
-module.exports = withLess({
-  lessLoaderOptions: {
-    lessOptions: {
-      modifyVars: {
-        'primary-color': '#333',
-        'heading-color': '#212121',
-        'text-color': '#212121',
-        'link-color': '#1890ff',
-        'font-size-base': '14px',
-        'border-radius-base': '4px',
-        'box-shadow-base': '0 2px 4px rgb(0 0 0 / 8%)',
-        'font-family': 'Gordita, sans-serif',
+const plugins = [
+  [
+    withLess,
+    {
+      lessLoaderOptions: {
+        lessOptions: {
+          modifyVars: {
+            'primary-color': '#333',
+            'heading-color': '#212121',
+            'text-color': '#212121',
+            'link-color': '#1890ff',
+            'font-size-base': '14px',
+            'border-radius-base': '4px',
+            'box-shadow-base': '0 2px 4px rgb(0 0 0 / 8%)',
+            'font-family': 'Gordita, sans-serif',
+          },
+        },
       },
     },
-  },
-})
+  ],
+]
+
+module.exports = withPlugins(plugins, nextConfig)
