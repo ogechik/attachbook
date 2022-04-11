3000f7ecdf0c9c16aaadd5f69b0ee6c2ff364fd3
reptile <kevinogechi798@gmail.com>
2022-04-11 12:46:06 +0300

    logout api

---
+++ pages/api/auth/logout.js
@@ -0,0 +1,16 @@
+import cookie from 'cookie'
+
+export default async (req, res) => {
+  try {
+    res.setHeader('Set-Cookie', [
+      cookie.serialize('auth', '', {
+        maxAge: -1,
+        path: '/',
+      }),
+    ])
+
+    return res.status(204).json({ msg: 'logged out' })
+  } catch (e) {
+    return res.status(500).json({ error: 'something went wrong' })
+  }
+}
