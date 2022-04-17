c6c9299f922fea78550539b20a5a170d941d8a81
reptile <kevinogechi798@gmail.com>
2022-04-17 11:42:32 +0300

    logbook view restrict

--- pages/student/logbook.js
+++ pages/student/logbook.js
@@ -1,7 +1,17 @@
-import { Typography, Row, Col, Tabs, Collapse, Divider } from 'antd'
+import {
+  Typography,
+  Row,
+  Col,
+  Tabs,
+  Collapse,
+  Divider,
+  Alert,
+  Button,
+} from 'antd'
 import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import StudentNav from '../../components/student/navigation'
-import Logbox from '../../components/logbook/Logbox'
+import LogBox from '../../components/logbook/Logbox'
 import ReviewBox from '../../components/logbook/ReviewBox'
 import SwitchBox from '../../components/logbook/SwitchBox'
 const { Text } = Typography
@@ -9,12 +19,13 @@ const { TabPane } = Tabs
 const { Panel } = Collapse
 
 export default function StudentHome({ book }) {
+  const router = useRouter()
   const [logbook, setLogbook] = useState(book)
-  const [week, setWeek] = useState(logbook.report.length - 1)
-  const [report, setReport] = useState(logbook.report[week])
+  const [week, setWeek] = useState(logbook?.report.length - 1)
+  const [report, setReport] = useState(logbook?.report[week])
 
   useEffect(() => {
-    setReport(logbook.report[week])
+    setReport(logbook?.report[week])
   }, [logbook, week])
 
   const doneIcon = (log) => {
@@ -32,106 +43,119 @@ export default function StudentHome({ book }) {
   return (
     <>
       <StudentNav />
-      <Row>
-        <Col xs={{ span: 22, offset: 1 }} lg={{ span: 16, offset: 4 }}>
-          <Row>
-            <Col span={24}>
-              <Text strong>
-                {`${logbook.student.firstName} ${logbook.student.lastName}`}
-              </Text>
-            </Col>
-            <Col span={24}>
-              <Text>{logbook.registrationNo.toUpperCase()}</Text>
-            </Col>
-            <Col span={24} style={{ marginTop: '1.25rem' }}>
-              <SwitchBox
-                week={week}
-                setWeek={setWeek}
-                logbook={logbook}
-                setLogbook={setLogbook}
-              />
-            </Col>
-            <Col span={24}>
-              <Tabs defaultActiveKey="1">
-                <TabPane tab="Logs" key="1">
-                  <Collapse defaultActiveKey={['1']}>
-                    {report.logs.map((log) => (
-                      <Panel
-                        header={log.day}
-                        key={log._id}
-                        extra={doneIcon(log)}
-                      >
-                        <Logbox
-                          log={log}
-                          logbookId={logbook._id}
-                          reportId={report._id}
-                          setLogbook={setLogbook}
-                        />
-                      </Panel>
-                    ))}
-                  </Collapse>
-                </TabPane>
-                <TabPane tab="Weekly Reviews" key="2">
-                  <ReviewBox
-                    logbookId={logbook._id}
-                    reportId={report._id}
-                    setLogbook={setLogbook}
-                    review={report?.review?.reviewText}
-                  />
-                  <Divider />
-                  {report.supervisorReview && (
-                    <>
-                      <Text strong>Supervisor's Review</Text>
-                      <p>{report?.supervisorReview?.reviewText}</p>
-                    </>
-                  )}
-                </TabPane>
-                <TabPane tab="Lecturer Comments" key="3">
-                  <Row>
-                    <Col>
-                      <Text strong>First visit comments</Text>
-                      <p>
-                        {logbook.firstComment ? (
-                          logbook.firstComment.comment
-                        ) : (
-                          <Text type="secondary">-</Text>
-                        )}
-                      </p>
-                    </Col>
-                  </Row>
-                  <Divider />
-                  <Row>
-                    <Col>
-                      <Text strong>Final comments</Text>
-                      <p>
-                        {logbook.finalComment ? (
-                          logbook.finalComment.comment
-                        ) : (
-                          <Text type="secondary">-</Text>
-                        )}
-                      </p>
-                    </Col>
-                  </Row>
-                </TabPane>
-              </Tabs>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+      {logbook ? (
+        <Row>
+          <Col xs={{ span: 22, offset: 1 }} lg={{ span: 16, offset: 4 }}>
+            <Row>
+              <Col span={24}>
+                <Text strong>
+                  {`${logbook.student.firstName} ${logbook.student.lastName}`}
+                </Text>
+              </Col>
+              <Col span={24}>
+                <Text>{logbook.registrationNo.toUpperCase()}</Text>
+              </Col>
+              <Col span={24} style={{ marginTop: '1.25rem' }}>
+                <SwitchBox
+                  week={week}
+                  setWeek={setWeek}
+                  logbook={logbook}
+                  setLogbook={setLogbook}
+                />
+              </Col>
+              <Col span={24}>
+                <Tabs defaultActiveKey="1">
+                  <TabPane tab="Logs" key="1">
+                    <Collapse defaultActiveKey={['1']}>
+                      {report.logs.map((log) => (
+                        <Panel
+                          header={log.day}
+                          key={log._id}
+                          extra={doneIcon(log)}
+                        >
+                          <LogBox
+                            log={log}
+                            logbookId={logbook._id}
+                            reportId={report._id}
+                            setLogbook={setLogbook}
+                          />
+                        </Panel>
+                      ))}
+                    </Collapse>
+                  </TabPane>
+                  <TabPane tab="Weekly Reviews" key="2">
+                    <ReviewBox
+                      logbookId={logbook._id}
+                      reportId={report._id}
+                      setLogbook={setLogbook}
+                      review={report?.review?.reviewText}
+                    />
+                    <Divider />
+                    {report.supervisorReview && (
+                      <>
+                        <Text strong>Supervisor's Review</Text>
+                        <p>{report?.supervisorReview?.reviewText}</p>
+                      </>
+                    )}
+                  </TabPane>
+                  <TabPane tab="Lecturer Comments" key="3">
+                    <Row>
+                      <Col>
+                        <Text strong>First visit comments</Text>
+                        <p>
+                          {logbook.firstComment ? (
+                            logbook.firstComment.comment
+                          ) : (
+                            <Text type="secondary">-</Text>
+                          )}
+                        </p>
+                      </Col>
+                    </Row>
+                    <Divider />
+                    <Row>
+                      <Col>
+                        <Text strong>Final comments</Text>
+                        <p>
+                          {logbook.finalComment ? (
+                            logbook.finalComment.comment
+                          ) : (
+                            <Text type="secondary">-</Text>
+                          )}
+                        </p>
+                      </Col>
+                    </Row>
+                  </TabPane>
+                </Tabs>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      ) : (
+        <Row>
+          <Col xs={{ span: 22, offset: 1 }} lg={{ span: 16, offset: 4 }}>
+            <Alert
+              message="Setup your attachment session."
+              showIcon
+              description="You need to setup your attachment session before accessing your logbook."
+              type="warning"
+            />
+            <Divider />
+            <Button
+              size="large"
+              onClick={() => router.push('/student')}
+              type="primary"
+            >
+              Setup Session
+            </Button>
+          </Col>
+        </Row>
+      )}
     </>
   )
 }
 
 export async function getServerSideProps(context) {
   const { cookie } = context.req.headers
-  if (!cookie) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: '/',
-      },
-    }
-  }
 
   const response = await fetch(`${process.env.DOMAIN}/api/s/logbook`, {
     headers: { cookie },
@@ -142,6 +166,13 @@ export async function getServerSideProps(context) {
       redirect: {
         permanent: false,
         destination: '/',
+      },
+    }
+  }
+  if (response.status === 404) {
+    return {
+      props: {
+        book: null,
       },
     }
   }
